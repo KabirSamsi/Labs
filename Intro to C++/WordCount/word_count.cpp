a4ba03cde9e3b9f6350383b50ba7a364aee70e12
Kabir Samsi <samsikabir@gmail.com>
2021-02-26 12:00:57 -0800

    Refactoring, security updates

--- Intro to C++/WordCount/word_count.cpp
+++ Intro to C++/WordCount/word_count.cpp
@@ -78,7 +78,7 @@ int countWords(string phrase) {
     for (int i = 0; i < expression.length(); i++) {
         if (isIncluded(alphanumeric, tolower(expression[i]))) { //If character is alphanumeric, add it to word
             word += expression[i];
-        } else if (word != "") { //If the end of the word has been reached, and the word has some characters, increment wordcount
+        } else if (word != "") { //If the word end has been reached, and the word has some characters, increment wordcount
             wordCount ++;
             word = "";
         }
