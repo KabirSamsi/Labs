dc01ddc7cc6d2b63c02f9395d346eaead7d27da7
Kabir Samsi <samsikabir@gmail.com>
2021-04-05 19:35:08 -0700

    Removed unnecessary features from lab 8

--- Intro to C++/VectorMerge/vectormerge.cpp
+++ Intro to C++/VectorMerge/vectormerge.cpp
@@ -1,8 +1,6 @@
 #include <iostream>
-#include <string>
 #include <iomanip>
 #include <vector>
-#include <sstream>
 
 using namespace std;
 
@@ -21,11 +19,22 @@ void printMeFirst(string name, string courseInfo) {
     cout << "Date: " << ctime(&now) << endl;
 }
 
+/**
+    @Purpose - function merges two sorted vectors and then sorts them
+    @param a - Sorted vector of integers
+    @param b - Sorted vector of integers
+    @return - Sorted vector of integers that combines the two vector parameters
+    @author - Kabir Samsi
+**/
+
 vector<int> merge_sorted(vector<int>  a, vector<int> b) {
-    vector<int> merged;
+    vector<int> merged; //Vector stores the merged and sorted vector
+
+    //Concatenate vectors
     for (int i = 0; i < a.size(); i++) {merged.push_back(a[i]);}
     for (int i = 0; i < b.size(); i++) {merged.push_back(b[i]);}
 
+    //Selection sort algorithm sorts the two concatenated vectors
     int temp;
     for (int i = 0; i < merged.size(); i++) {
         for (int j = 0; j < merged.size()-1; j++) {
@@ -36,29 +45,38 @@ vector<int> merge_sorted(vector<int>  a, vector<int> b) {
             }
         }
     }   
-    return merged;
+    return merged; //Return merged & sorted vector
 }
 
-int main() {
-    string input1, input2;
-    int number;
-    vector<int> g1, g2;
+/**
+    @Purpose - function builds two sorted vectors and then merges them, implemnting the merge_sorted function
+    @param - None
+    @return - int (0)
+    @author - Kabir Samsi
+**/
 
+int main() {
+    vector<int> g1, g2;
     printMeFirst("Kabir Samsi", "Lab 8 - Vector Merge");
 
-    cout << "Vector A:" << endl;
-    getline(cin, input1);
-    std::stringstream iss(input1);
-    while (iss >> number) { g1.push_back( number );}
+    //Test Case Vector 1
+    cout << "Vector a: " << endl;
+    for (int i = 0; i < 7; i ++) {
+        cout << i*i << " ";
+        g1.push_back(i * i);
+    }
 
-    cout << "\nVector B:" << endl;
-    getline(cin, input2);
-    std::stringstream iss2(input2);
-    while (iss2 >> number) { g2.push_back( number);}
+    //Test Case Vector 2
+    cout << endl << "\nVector b: " << endl;
+    for (int i = 0; i < 5; i ++) {
+        cout << (i*i)+4 << " ";
+        g2.push_back((i * i) + 4);
+    }
 
+    //Merge Vvctor and output result
     vector<int> merged = merge_sorted(g1, g2);
-    cout << "\nResult of interleaved merge of sorted a and b is\n";
-    for (int i = 0; i < merged.size(); i++) {cout << merged[i] << " ";}
+    cout << endl << "\nResult of interleaved merge of sorted a and b is\n";
+    for (int i = 0; i < merged.size(); i++) cout << merged[i] << " ";
     cout << endl;
 
     return 0;
